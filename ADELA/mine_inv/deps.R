9e62ab84fa8ed47d35b8e7cd06b24ff4152584f3
Andres Gomez Urquiza <andres@fractal.ai>
2016-02-11 18:32:30 -0600

    deps de get_inv_class.R

---
+++ ADELA/mine_inv/deps.R
@@ -0,0 +1,14 @@
+#! /usr/bin/Rscript
+
+install.packages("lubridate")
+install.packages("stringr")
+install.packages("stringdist")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("data.table")
+install.packages("ggplot2")
+install.packages("gdata")
+install.packages("readxl")
+install.packages("xlsx")
+install.packages("foreign")
